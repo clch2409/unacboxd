30dac1bbdcbe6b6ec5ddd5535703f6a23ec61607
clch2409 <cesarlch2409@gmail.com>
2026-09-13 21:43:27 -0500

    Modificando un 80% de la pantalla principal, agregando variables css, animaciones, interacción con el usuario al hacer hover y estilos con la paleta de colores escogida

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import peliculas from "./peliculas.json" with { type: "json" };
-// Funciona
+// // Funciona
+// peliculas.forEach((peli) => console.log(peli));
+
+// // JSON -> Javascript Object Notation
+
+let persona = {
+  nombre: "Cesar",
+  edad: 24,
+};
+
+console.log(
+  `La persona se llama ${persona.nombre} y tiene una edad de ${persona.edad}`,
+);
