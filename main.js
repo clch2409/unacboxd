37a6551dd8c028da9ae697b03d2bb06667d18962
clch2409 <cesarlch2409@gmail.com>
2026-09-14 14:43:28 -0500

    Agregando el icono de filtrado con su respectivo menú de opciones, mejorando la animación de las peliculas y agregando el html del dialog de las calificaciones

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 import peliculas from "./peliculas.json" with { type: "json" };
-// // Funciona
-// peliculas.forEach((peli) => console.log(peli));
-
-// // JSON -> Javascript Object Notation
-
-let persona = {
-  nombre: "Cesar",
-  edad: 24,
-};
-
-console.log(
-  `La persona se llama ${persona.nombre} y tiene una edad de ${persona.edad}`,
-);
