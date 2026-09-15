1177dfecb85c76c5480b211f04b8c9d26e788919
clch2409 <cesarlch2409@gmail.com>
2026-09-15 11:09:02 -0500

    Mejorando la colocación del elemento de favorito y agregando cambio de color en el corazón

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import peliculas from "./peliculas.json" with { type: "json" };
+
+const contenedor_img = document.querySelector(".contenedor-img");
+
+contenedor_img.addEventListener("click", (target) => {
+  if (target.target.tagName === "SPAN") {
+    target.target.classList.toggle("favourite");
+  }
+});
