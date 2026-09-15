8752aba205cc94fdff77d0ef8850370982f9c865
clch2409 <cesarlch2409@gmail.com>
2026-09-15 11:48:52 -0500

    Agregando animación al corazón

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import peliculas from "./peliculas.json" with { type: "json" };
 
+let peliculas_filtradas = [...peliculas];
+
 const contenedor_img = document.querySelector(".contenedor-img");
 
 contenedor_img.addEventListener("click", (target) => {
-  if (target.target.tagName === "SPAN") {
-    target.target.classList.toggle("favourite");
+  let imagen = target;
+  let corazon = imagen.target;
+
+  if (corazon.tagName == "SPAN") {
+    corazon.classList.toggle("favourite");
   }
 });
